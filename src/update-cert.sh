2300bc59e064874a9e537d232e19f105fc7b363e
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2021-04-07 10:48:27 -0400

    Create update-cert.sh

---
+++ src/update-cert.sh
@@ -0,0 +1,29 @@
+source="https://homelab.express/cert/"
+cer="wildcard.homelab.express.cer"
+key="wildcard.homelab.express.key"
+
+# download the new files
+wget "$source$cer" -O "/$cer"
+wget "$source$key" -O "/$key"
+
+# check if they differ from the existing files (starting with key, since it's shorter)
+matches=false
+if cmp -s -- "/$key" "/certs/$key"; then
+  if cmp -s -- "/$cer" "/certs/$cer"; then
+    matches=true
+  fi
+fi
+
+# update files if the new ones differ
+if [ "$matches" = false ]; then
+  # back up current files
+  mv "/certs/$cer" "/certs/$cer.bak"
+  mv "/certs/$key" "/certs/$key.bak"
+
+  # copy the new files
+  mv "/$cer" "/certs/$cer"
+  mv "/$key" "/certs/$key"
+
+  # touch dynamic.toml to alert that we've updated the cert
+  touch /dynamic.toml
+fi
