7eba44675c54d2ec83aead6a9c871c4563b46cd6
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2021-04-08 15:24:09 -0400

    Update update-cert.sh

--- update-cert.sh
+++ update-cert.sh
@@ -2,28 +2,37 @@ source="https://homelab.express/cert/"
 cer="wildcard.homelab.express.cer"
 key="wildcard.homelab.express.key"
 
-# download the new files
-wget "$source$cer" -O "/$cer"
-wget "$source$key" -O "/$key"
+# not wild about doing it this way, but it works
 
-# check if they differ from the existing files (starting with key, since it's shorter)
-matches=false
-if cmp -s -- "/$key" "/certs/$key"; then
-  if cmp -s -- "/$cer" "/certs/$cer"; then
-    matches=true
+while true; do
+
+  # download the new files
+  wget "$source$cer" -O "/$cer"
+  wget "$source$key" -O "/$key"
+
+  # check if they differ from the existing files (starting with key, since it's shorter)
+  matches=false
+  if cmp -s -- "/$key" "/certs/$key"; then
+    if cmp -s -- "/$cer" "/certs/$cer"; then
+      matches=true
+    fi
   fi
-fi
 
-# update files if the new ones differ
-if [ "$matches" = false ]; then
-  # back up current files
-  mv "/certs/$cer" "/certs/$cer.bak"
-  mv "/certs/$key" "/certs/$key.bak"
+  # update files if the new ones differ
+  if [ "$matches" = false ]; then
+    # back up current files
+    mv "/certs/$cer" "/certs/$cer.bak"
+    mv "/certs/$key" "/certs/$key.bak"
 
-  # copy the new files
-  mv "/$cer" "/certs/$cer"
-  mv "/$key" "/certs/$key"
+    # copy the new files
+    mv "/$cer" "/certs/$cer"
+    mv "/$key" "/certs/$key"
 
-  # touch dynamic.toml to alert that we've updated the cert
-  touch /dynamic.toml
-fi
+    # touch dynamic.toml to alert that we've updated the cert
+    touch /dynamic.toml
+  fi
+
+  # pack it in for the day
+  sleep 86400
+
+done
